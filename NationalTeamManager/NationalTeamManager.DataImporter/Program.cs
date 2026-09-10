24878e183931131dc9ba4f3a81671e7257dc4e5e
Tamás Boldizsár Bihari <biharitb@mvmi.hu>
2026-09-10 11:39:21 +0200

    chore: set up backend foundation

--- NationalTeamManager/NationalTeamManager.DataImporter/Program.cs
+++ NationalTeamManager/NationalTeamManager.DataImporter/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+using NationalTeamManager.Infrastructure.DependencyInjection;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+using var host = builder.Build();
+
+await host.RunAsync();
